f23a7bfedd57b21c4883d2e569c777471e7181a6
Esraa-Hassan0 <esraa.elfatairy04@eng-st.cu.edu.eg>
2024-12-23 18:51:23 +0200

    fix error

--- main.asm
+++ main.asm
@@ -80,7 +80,7 @@
     STEP_PER_ROW              EQU 40                                      ;(BRICK_WIDTH+1PX SPACE)
     STEP_PER_COL              EQU 12                                      ;(BRICK_WIDTH+1PX SPACE)
 
-    COLOR_MATRIX              db  11 dup (4,4,9,9)                    ; EACH Brick must have certain color here
+    COLOR_MATRIX              db  11 dup (4,4,9,9)                        ; EACH Brick must have certain color here
     
     GNCLR_MATRIX              db  11 dup (4,4,9,9)
 
@@ -1896,43 +1896,43 @@ RESET_GAME PROC
     ; MOV  BALL_COLOR, 00H
     ; CALL DRAWING_BALL
     ; MOV  BALL_COLOR, 0FH
-    MOV BALL_POSITION_X  ,      160D                                 
-    MOV BALL_POSITION_Y  ,      185D   
-    MOV BALL_SPEED_Y     ,       5H                                      ;THE SPEED OF THE BALL IN Y DIRECTION
-    MOV BALL_SPEED_X      ,      2H
-     mov   BALL_POWER_UP  , 0
-    mov  BALL_POWER_DOWN     , 0
+                               MOV  BALL_POSITION_X  ,      160D
+                               MOV  BALL_POSITION_Y  ,      185D
+                               MOV  BALL_SPEED_Y     ,       5H        ;THE SPEED OF THE BALL IN Y DIRECTION
+                               MOV  BALL_SPEED_X      ,      2H
+                               mov  BALL_POWER_UP  , 0
+                               mov  BALL_POWER_DOWN     , 0
 
     
     ; CALL MOVING_BALL
 
     ; CALL DRAWING_BALL
 
-    MOV width_Paddle     ,      50d
-    MOV height_Paddle    ,      4d
-    MOV Paddle_Speed     ,      6
-    CALL clear_Paddle
-    MOV Paddle_X         ,      135D
-    MOV Paddle_Y         ,      196D
-    CALL Draw_Paddle
+                               MOV  width_Paddle     ,      50d
+                               MOV  height_Paddle    ,      4d
+                               MOV  Paddle_Speed     ,      6
+                               CALL clear_Paddle
+                               MOV  Paddle_X         ,      135D
+                               MOV  Paddle_Y         ,      196D
+                               CALL Draw_Paddle
 
-;     ;RESET_CLR_MATRIX
+    ;     ;RESET_CLR_MATRIX
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    MOV DI,0
-   mov   ROW      ,       FIRST_ROW_POS
-   mov COL        ,     FIRST_COL_POS
-   mov CRNT_BRICK ,     0    
-    RESET_CLR_MTRX:
-    MOV AL,[GNCLR_MATRIX+DI]
-    MOV [COLOR_MATRIX+DI],AL
-    INC DI
-    CMP DI,33
-    JNZ RESET_CLR_MTRX
-;     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    MOV IsPowerDown_pre,0
-    MOV IsPowerUp_pre,0
-    mov score,0
-   RET
+                               MOV  DI,0
+                               mov  ROW      ,       FIRST_ROW_POS
+                               mov  COL        ,     FIRST_COL_POS
+                               mov  CRNT_BRICK ,     0
+    RESET_CLR_MTRX:            
+                               MOV  AL,[GNCLR_MATRIX+DI]
+                               MOV  [COLOR_MATRIX+DI],AL
+                               INC  DI
+                               CMP  DI,33
+                               JNZ  RESET_CLR_MTRX
+    ;     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                               MOV  IsPowerDown_pre,0
+                               MOV  IsPowerUp_pre,0
+                               mov  score,0
+                               RET
 RESET_GAME ENDP
 
 end main
